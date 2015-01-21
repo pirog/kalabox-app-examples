e9dd048122b24156ba7a4b51a57458ea9e03d647
Mike Pirog <mike@kalamuna.com>
2015-01-21 13:25:22 -0800

    #183: Use /data/code for webroot

--- drupal7/dockerfiles/drupal7/app/start.sh
+++ drupal7/dockerfiles/drupal7/app/start.sh
@@ -6,15 +6,11 @@ if [ ! -f /data/code/sites/default/settings.php ]; then
   # Generate random passwords
   DRUPAL_DB='kalabox'
   MYSQL_PASSWORD='kalabox'
-  DRUPAL_PASSWORD='kalabox'
   # This is so the passwords show up in logs.
   echo mysql root password: $MYSQL_PASSWORD
-  echo drupal password: $DRUPAL_PASSWORD
   echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $DRUPAL_PASSWORD > /drupal-db-pw.txt
   mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE kalabox; GRANT ALL PRIVILEGES ON kalabox.* TO 'kalabox'@'%' IDENTIFIED BY '$DRUPAL_PASSWORD'; FLUSH PRIVILEGES;"
-  sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/sites-available/default
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE kalabox; GRANT ALL PRIVILEGES ON kalabox.* TO 'kalabox'@'%' IDENTIFIED BY 'KALABOX'; FLUSH PRIVILEGES;"
   a2enmod rewrite vhost_alias
   killall mysqld
   sleep 10s
