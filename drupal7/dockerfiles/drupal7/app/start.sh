b4345ab28b5e640e642e170e2d755e425e06391a
Mike Pirog <mike@kalamuna.com>
2015-01-21 14:01:16 -0800

    #183: Update PHP for BIG DRUPAL, fix mysql kalabox user

--- drupal7/dockerfiles/drupal7/app/start.sh
+++ drupal7/dockerfiles/drupal7/app/start.sh
@@ -4,13 +4,12 @@ if [ ! -f /data/code/sites/default/settings.php ]; then
   /usr/bin/mysqld_safe &
   sleep 10s
   # Generate random passwords
-  DRUPAL_DB='kalabox'
   MYSQL_PASSWORD='kalabox'
   # This is so the passwords show up in logs.
   echo mysql root password: $MYSQL_PASSWORD
   echo $MYSQL_PASSWORD > /mysql-root-pw.txt
   mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE kalabox; GRANT ALL PRIVILEGES ON kalabox.* TO 'kalabox'@'%' IDENTIFIED BY 'KALABOX'; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE kalabox; GRANT ALL PRIVILEGES ON kalabox.* TO 'kalabox'@'%'; FLUSH PRIVILEGES;"
   a2enmod rewrite vhost_alias
   killall mysqld
   sleep 10s
