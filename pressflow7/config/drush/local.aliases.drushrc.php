b6d27baacec2ea91919709e8bde43554c2134d6f
Mike Pirog <mike@kalamuna.com>
2015-02-19 14:00:57 -0800

    Upgrade plugins

--- pressflow7/config/drush/local.aliases.drushrc.php
+++ pressflow7/config/drush/local.aliases.drushrc.php
@@ -2,9 +2,9 @@
 /**
  * This file is automatically updated by kalabox-plugin-drush.
  */
-$aliases['" + app.name + "'] = array(
+$aliases['pressflow7'] = array(
   'root' => '/Users/mpirog/kalabox/code/pressflow7',
   'uri' => 'pressflow7.kbox',
-  'db-url' => 'mysql://kalabox@pressflow7.kbox:49158/kalabox',
+  'db-url' => 'mysql://kalabox@pressflow7.kbox:49153/kalabox',
   'db-allows-remote' => TRUE
 );
