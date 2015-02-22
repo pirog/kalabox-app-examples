4cc66b3e85b2f14aaa16741e676a04dbe074ce80
Mike Pirog <mike@kalamuna.com>
2015-02-21 16:41:06 -0800

    #205: First pass on backdrop with drush support

---
+++ backdrop/config/drush/local.aliases.drushrc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is automatically updated by kalabox-plugin-drush.
+ */
+$aliases['backdrop'] = array(
+  'root' => '/Users/mpirog/kalabox/code/backdrop',
+  'uri' => 'backdrop.kbox',
+  'db-url' => 'mysql://kalabox@backdrop.kbox:49233/kalabox',
+  'db-allows-remote' => TRUE
+);
