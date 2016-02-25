448e9e62f91f6219105e5d8b6220efb811eb3d0a
Mike Pirog <mike@kalamuna.com>
2016-02-24 20:38:03 -0800

    Clean out old stuff

--- hiphop-drupal7/config/nginx/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-HHVM_HOST=$(dig +short hhvm.${APPDOMAIN})
-sed -i "s/fastcgi_pass .*/fastcgi_pass ${HHVM_HOST}:9000;/g" /src/config/nginx/site.conf
-sed -i "s/PRESSFLOW_SETTINGS .*/PRESSFLOW_SETTINGS '${KB_APP_SETTINGS}';/g" /src/config/nginx/site.conf
-
-nginx
